b9ca5104796e88f5bccc0625f6320afbba8d00a8
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-23 13:24:55 +0530

    [Ritesh]add : Given Message

---
+++ MoodAnaluserPrograms/MoodAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnaluserPrograms
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string CheckMoodAnalyser()
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
---
+++ MoodAnaluserPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnaluserPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyser Program");
+            MoodAnalyser moodAnalyser = new MoodAnalyser("Iam in Happy mood");
+            Console.WriteLine(moodAnalyser.CheckMoodAnalyser());
+            Console.ReadLine();
+        }
+    }
+}
